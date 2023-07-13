b664267654b3d605246b1aa4e85715435111eafb
tameesoul <tamerghazy@std.mans.edu.eg>
2023-07-13 02:48:02 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-#include<stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
